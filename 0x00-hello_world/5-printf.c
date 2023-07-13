d57f7a8f61afbf8ff32eda9f7146258fc5b7174f
Cybill U <cybillu@gmail.com>
2023-07-13 16:18:16 -0700

    Task 5: Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main program entry. Print a set of characters
+ * Return: returns 0 for success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
