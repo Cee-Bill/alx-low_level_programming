27331b535c3de20770476f8b14b0530a7b62ad2b
Cybill U <cybillu@gmail.com>
2023-07-13 18:38:27 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- prints a set of characters
+ * Return: returns 1 for error
+*/
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
