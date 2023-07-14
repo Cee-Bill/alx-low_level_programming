621bf46758c3e57a98cecf8f467800eaaca891cb
Cybill U <cybillu@gmail.com>
2023-07-14 15:16:43 -0700

    Task 6: print 0 to 9 with putchar2

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
-* main - prints all single digit numbers of base 10 starting from 0
-* Return; return 0 for success
+* main- prints all single digit numbers of base 10 starting from 0
+* Return: return 0 for success
 */
 int main(void)
 {
