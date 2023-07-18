48d7cea1bafda71ebb5d6b2f93eb77c9f30eeae1
Cybill U <cybillu@gmail.com>
2023-07-18 16:48:27 -0700

    Task 0: 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main- program entry
  * Description - prints _putchar
@@ -7,9 +7,15 @@
 
 int main(void)
 {
-	char n[] = "_putchar";
-
-	printf("%s\n", n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
