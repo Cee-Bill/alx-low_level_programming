e6a44e79e308b94a6267df25f3eaaec4eb24737d
Cybill U <cybillu@gmail.com>
2023-07-18 17:52:31 -0700

    Task 1: lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet- prints the alphabet in lowercase
+ * Return: returns 0
+ */
+void print_alphabet(void)
+{
+	int alph = 97;
+
+	while (alph <= 122)
+	{
+		_putchar('alph');
+		alph++;
+	}
+	_putchar('\n');
+}
