823eba71dbf25b89abee257ccd8d4b915df2258f
Cybill U <cybillu@gmail.com>
2023-07-19 19:57:04 -0700

    Task 10: adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @i: expected integer param
+ * @j: 2nd expected param
+ * Return: the result of the sum
+ */
+
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+	return (result);
+}
