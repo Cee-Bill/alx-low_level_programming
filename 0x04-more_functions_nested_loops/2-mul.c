aec807975a88f96b9a429d5dd739cf0f0950369a
Cybill U <cybillu@gmail.com>
2023-07-20 17:30:13 -0700

    Task 2: multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: expected integer parameter 1
+ * @b: expected param 2
+ * Return: result value
+ */
+int mul(int a, int b)
+{
+	int resval = a * b;
+
+	return (resval);
+}
