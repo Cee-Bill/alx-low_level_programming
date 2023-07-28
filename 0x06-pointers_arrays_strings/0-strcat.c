b047eb666683aae5eb9972e6bea3b8fdc139df7f
Cybill U <cybillu@gmail.com>
2023-07-27 18:09:49 -0700

    Task 0 strcat-file update hotfix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest - param 1
- * @src - param 2
+ * @dest: param 1
+ * @src: param 2
  * Return: string
  */
 
@@ -12,7 +12,9 @@ char *_strcat(char *dest, char *src)
 	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
-	       	i++;
+	{
+		i++;
+	}
 
 	while (src[j] != '\0')
 	{
