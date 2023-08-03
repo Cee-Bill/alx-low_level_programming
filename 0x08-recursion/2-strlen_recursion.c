fdecc98429da2aab7bbd30d5b1ea565f5a2ba4ab
Cybill U <cybillu@gmail.com>
2023-08-03 13:47:06 -0700

    Task 2: Dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s string to be counted
+* Return: length- interger
+*/
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s == '\0')
+		return (0);
+
+	if (*s != '\0')
+	{
+		n++;
+		return (n + _strlen_recursion(s + 1));
+
+	}
+	return (n);
+}
