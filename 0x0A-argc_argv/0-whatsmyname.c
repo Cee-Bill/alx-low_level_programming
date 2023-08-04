db9e5460d50dc43363e3bcc2a2670c28c54d8693
Cybill U <cybillu@gmail.com>
2023-08-04 14:51:04 -0700

    Task 0- It ain't what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: integer param for argument count
+ * @argv: char array holding app passed arguments
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
