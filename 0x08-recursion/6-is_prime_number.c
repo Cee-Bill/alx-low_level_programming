18fedba4d92426037eace5f9cc83fea9415b5958
Cybill U <cybillu@gmail.com>
2023-08-03 18:11:23 -0700

    Task 6: Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is a prime
+ * @n: the numcer to be checked
+ * Return: returns 1 if the input integer is a prime number
+ * Otherwise, 0
+ */
+
+int prime_check(int n, int i);
+
+int is_prime_number(int n)
+{
+	return (prime_check(n, 1));
+}
+
+int prime_check(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if ((n % i == 0) && (i > 1))
+		return (0);
+	if ((n / i) < i)
+		return (1);
+	return (prime_check(n, i + 1));
+}
