69738ebde6f9269b76e6a28b4ade02721d7c741b
Cybill U <cybillu@gmail.com>
2023-08-03 19:53:39 -0700

    Task 6: betty corrections 2

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,25 @@
 #include "main.h"
-
 /**
- * is_prime_number - checks if a number is a prime
- * prime_check - preliminary func
+  * is_prime_number - checks if a number is a prime
  * @i: int t
  * @n: the numcer to be checked
  * Return: returns 1 if the input integer is a prime number
  * Otherwise, 0
  */
-
 int prime_check(int n, int i);
 
 int is_prime_number(int n)
 {
 	return (prime_check(n, 1));
 }
+
+/**
+ * prime_check - preliminary func
+ * @n: the numcer to be checked
+ * @i: counter
+ * Return: returns 1 if the input integer is a prime number
+ * Otherwise, 0
+ */
 
 int prime_check(int n, int i)
 {
