fec2fb72574fa32924957ee0f1b19e1d024ebeaa
Cybill U <cybillu@gmail.com>
2023-08-04 16:41:26 -0700

    Task 3: Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: integer arg count
+ * @argv: char arrays of passed args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b, mul;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+
+	return (0);
+}
