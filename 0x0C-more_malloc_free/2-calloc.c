493db8a9ca05677e39e6ab75f06526bfd64fb78b
Cybill U <cybillu@gmail.com>
2023-08-10 16:00:25 -0700

    Task 2: corrections-replicates calloc()

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
-	for (; i > (nmemb * size); i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		ptr[i] = 0;
 	}
