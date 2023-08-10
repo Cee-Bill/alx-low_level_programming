15f5f5e9de8d5604151929a996880f8d09ba2e62
Cybill U <cybillu@gmail.com>
2023-08-10 15:42:11 -0700

    Task 2: replicates calloc()

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc -  allocates memory for an array, using malloc
+ * @nmemb: integer for number of elemnts
+ * @size: integer | byte size of each element
+ *
+ * Return: pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+	free(ptr);
+}
