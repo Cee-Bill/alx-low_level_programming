62227bfb475a21f3bd73c6de60e82accb40ccc5c
Cybill U <cybillu@gmail.com>
2023-08-15 16:44:23 -0700

    Task 4: partial- A door is what a dog is...

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: member element 1 | char pointer
+ * @age: emeber element 2 | float
+ * @owner: memeber 3 | char pointer
+ *
+ * Return: struct dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+}
