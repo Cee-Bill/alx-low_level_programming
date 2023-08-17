477af429450da2d5543b6e76997983bd41ccf8b8
Cybill U <cybillu@gmail.com>
2023-08-17 13:26:58 -0700

    Task 0: What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to name of the person | char
+ * @f: pointer to a function that prints a name
+ *
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+	return;
+}
