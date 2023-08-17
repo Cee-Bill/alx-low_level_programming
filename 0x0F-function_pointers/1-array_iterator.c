b1d3318b2c6602f8825fe377a34885be7d0e95af
Cybill U <cybillu@gmail.com>
2023-08-17 13:53:25 -0700

    Task 1: f you spend too much time...

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a param on each
+ * element of an array
+ * @array: the array elements to be eexcuted
+ * @size: array size
+ * action: pointer to the function to be used
+ *
+ * Return: void.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array && size && action)
+	{
+		for (; i <= size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
