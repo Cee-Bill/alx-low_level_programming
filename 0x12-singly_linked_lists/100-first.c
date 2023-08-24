dcabded9e4f93847d89d850afe648e029a4e9d67
Cybill U <cybillu@gmail.com>
2023-08-24 15:09:05 -0700

    Task 5: betty fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void first() __attribute__((constructor));
+void first(void) __attribute__((constructor));
 
 /**
  * first - prints a string before the main function is executed.
