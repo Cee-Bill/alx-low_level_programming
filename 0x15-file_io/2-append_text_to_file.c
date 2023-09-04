81ada32853e10bcd3ab5d5731f29eed029da7e92
Cybill U <cybillu@gmail.com>
2023-09-04 16:41:36 -0700

    Task 2: betty fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[_len])
-		       _len++;
+			_len++;
 	}
 
 	file_op = open(filename, O_WRONLY | O_APPEND);
