2905df0707bc83774ef0eee374cd49592d476db4
cee-bill <cybillu@gmail.com>
2023-09-28 12:22:41 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,20 +8,20 @@
 
 void print_binary(unsigned long int n)
 {
-  int i;
-  int num = 0;
-  unsigned long int position;
+	int i;
+	int num = 0;
+	unsigned long int position;
 
-  for (i = 63; i >= 0; i--)
-    {
-      position = n >> i;
+	for (i = 63; i >= 0; i--)
+	{
+		position = n >> i;
 
-      if (position & 1)
-	_putchar('1'), num++;
+		if (position & 1)
+			_putchar('1'), num++;
 
-      else if (num)
-	_putchar('0');
-    }
-  if (!num)
-    _putchar('0');
+		else if (num)
+			_putchar('0');
+	}
+	if (!num)
+		_putchar('0');
 }
