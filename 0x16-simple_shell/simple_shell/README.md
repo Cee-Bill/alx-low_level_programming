e6ec1fac2a3208b93212a9f6f27d6c0e72bb5725
cee-bill <cybillu@gmail.com>
2023-09-28 11:56:49 -0700

    remved repos

--- 0x16-simple_shell/simple_shell/README.md
+++
@@ -1,3 +0,0 @@
-## Task 0x16. C - Simple Shell
-
-A custom UNIX command line interpreter.
--- 0x16-simple_shell/simple_shell/versions/execute_cmd.c
+++
@@ -1,9 +0,0 @@
-#include "tsh.h"
-
-void execute_command(char *args[]) {
-    int exec_cmd = execve(args[0], args, NULL);
-    if (exec_cmd == -1) {
-        perror("execve failed");
-        exit(1);
-    }
-}
